b22e46c1a2888d6981165fdf1d002b21f4515bbf
Khurshidbek Sobirov <Khurshidddbek@gmail.com>
2023-04-05 04:27:11 +0500

    Fixed build bugs

--- android/app/src/main/kotlin/com/gotaxi/taxigo/taxigo/MainActivity.kt
+++ android/app/src/main/kotlin/com/gotaxi/taxigo/taxigo/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.gotaxi.taxigo.taxigo
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 import com.yandex.mapkit.MapKitFactory;
 
-public class MainActivity extends FlutterActivity {
-  @Override
-  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-    MapKitFactory.setApiKey("d4d58374-c106-4454-99f2-84721a5558ec"); // Your generated API key
+class MainActivity: FlutterActivity() {
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    MapKitFactory.setApiKey("d4d58374-c106-4454-99f2-84721a5558ec");
     super.configureFlutterEngine(flutterEngine);
   }
 }
